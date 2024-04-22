3092500324bdae1ab6cd0f738090caeb6fd4b9e6
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 15:52:07 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // write code here
+<script>
+        const product1 = {
+            name: 'Water',
+            distributor: {
+              name: 'Giraffe Water Company',
+              address: {
+                street: 'Phetchaburi',
+                subdistrict: 'Thanonphetchaburi',
+                district: 'Ratchathewi',
+                province: 'Bangkok'
+              }
+            }
+          };
+          console.log(product1.distributor.address.province);
+          
+          const product2 = {};
+          if (product2 && product2.distributor && product2.distributor.address) {
+            console.log(product2.distributor.address.province);
+          } else {
+            console.log('province of product2 not found'); // หากไม่พบ province
+          }
+          
+    </script>
